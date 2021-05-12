31fbe0bd1fcefbd49c392fba6fd68d08027b345a
user7832 <26787474+user7832@users.noreply.github.com>
2021-05-12 11:36:53 +0000

    Update variables.tf

--- temp-check/terraform_azure/variables.tf
+++ temp-check/terraform_azure/variables.tf
@@ -3,3 +3,14 @@ variable "location" {
     description = "Service location"
     default = "eastus"
 }
+
+variable "we_apikey" {
+    type = string
+    description = "API key for weatherapi.com service"
+}
+
+variable "we_city" {
+    type = string
+    description = "City which temprerature will be checked"
+    default = "Tallin"
+}
